e7a8fea443499ac4b05e7b559ae85af8d0f58448
Arjun Rajput <31860250+arjunrajput18@users.noreply.github.com>
2024-10-21 12:20:07 +0530

    fix :error

--- src/App.js
+++ src/App.js
@@ -5,10 +5,7 @@ import { Projects } from "./Pages/Projects";
 function App() {
   return (
     <div style={{ fontFamily: "Montserrat" }}>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/projects" element={<Projects />} />
-      </Routes>
+   <h1>hi</h1>
     </div>
   );
 }
